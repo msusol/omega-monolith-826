be6e45c97aa09f5def5cfc27e82e5be3d583ddac
marksusol <Mtmp9sus>
2015-01-20 19:10:58 -0800

    adding link to working demo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # omega-monolith-826
 Working Demo Project:  Google App Engine
+[http://omega-monolith-826.appspot.com/](http://omega-monolith-826.appspot.com/)
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('Hello Great Big World!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
